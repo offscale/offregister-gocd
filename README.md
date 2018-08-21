9f16ab38f346c642661d067c1e9a17fecc63f5f3
Samuel Marks <samuelmarks@gmail.com>
2018-08-21 14:55:33 +1000

    Init (alpha)

---
+++ README.md
@@ -0,0 +1,11 @@
+offregister_gocd
+===============
+This package follows the offregister specification to setup and configure GoCD
+
+## Install dependencies
+
+    pip install -r requirements.txt
+
+## Install package
+
+    pip install .
---
+++ offregister_gocd/ubuntu.py
@@ -0,0 +1,23 @@
+from fabric.contrib.files import exists, append
+from fabric.operations import sudo
+from offregister_fab_utils.apt import apt_depends
+
+
+def install_deps0(*args, **kwargs):
+    sudo('add-apt-repository -y ppa:webupd8team/java')
+    sudo('echo debconf shared/accepted-oracle-license-v1-1 select true | \
+  sudo debconf-set-selections')
+    apt_depends('oracle-java8-set-default')
+
+    srcs_list = '/etc/apt/sources.list.d/gocd.list'
+    if not exists(srcs_list):
+        append(srcs_list, 'deb https://download.gocd.org /', use_sudo=True)
+        sudo('curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo apt-key add -')
+
+
+def install_master1(*args, **kwargs):
+    apt_depends('go-server')
+
+
+def install_slave2(*args, **kwargs):
+    apt_depends('go-agent')
