568d8d02fe18fab5b9c6b6e39b734a64fc635aa2
Samuel Marks <samuelmarks@gmail.com>
2018-08-23 10:12:50 +1000

    0.0.1: Javac installs and so does nginx

--- offregister_gocd/ubuntu.py
+++ offregister_gocd/ubuntu.py
@@ -1,13 +1,17 @@
 from fabric.contrib.files import exists, append
 from fabric.operations import sudo
+
 from offregister_fab_utils.apt import apt_depends
+from offregister_fab_utils.fs import cmd_avail
+from offregister_nginx import ubuntu as nginx
 
 
 def install_deps0(*args, **kwargs):
-    sudo('add-apt-repository -y ppa:webupd8team/java')
-    sudo('echo debconf shared/accepted-oracle-license-v1-1 select true | \
-  sudo debconf-set-selections')
-    apt_depends('oracle-java8-set-default')
+    if not cmd_avail('javac'):
+        sudo('add-apt-repository -y ppa:webupd8team/java')
+        sudo('echo debconf shared/accepted-oracle-license-v1-1 select true | \
+      sudo debconf-set-selections')
+        apt_depends('oracle-java8-set-default')
 
     srcs_list = '/etc/apt/sources.list.d/gocd.list'
     if not exists(srcs_list):
@@ -21,3 +25,7 @@ def install_master1(*args, **kwargs):
 
 def install_slave2(*args, **kwargs):
     apt_depends('go-agent')
+
+
+def configure_nginx3(*args, **kwargs):
+    nginx.install_nginx0()
