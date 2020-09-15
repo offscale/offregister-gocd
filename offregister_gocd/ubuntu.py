c7e6889859184b614fecb6ac3a909afa637de657
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-15 15:11:03 +1000

    [README.md] New badges; [LICENSE-{APACHE,MIT}] Update copyright year range; [*.py] Python 3 support; Blacken; [requirements.txt,setup.py] Fix dependencies to reflect new names / Python 3

--- offregister_gocd/ubuntu.py
+++ offregister_gocd/ubuntu.py
@@ -1,4 +1,4 @@
-from cStringIO import StringIO
+from io import StringIO
 
 from fabric.contrib.files import exists, append
 from fabric.operations import sudo, put
@@ -12,52 +12,67 @@ from offregister_nginx import ubuntu as nginx
 
 
 def install_deps0(*args, **kwargs):
-    if not cmd_avail('javac'):
-        sudo('add-apt-repository -y ppa:webupd8team/java')
-        sudo('echo debconf shared/accepted-oracle-license-v1-1 select true | \
-      sudo debconf-set-selections')
-        apt_depends('oracle-java8-set-default')
+    if not cmd_avail("javac"):
+        sudo("add-apt-repository -y ppa:webupd8team/java")
+        sudo(
+            "echo debconf shared/accepted-oracle-license-v1-1 select true | \
+      sudo debconf-set-selections"
+        )
+        apt_depends("oracle-java8-set-default")
 
-    srcs_list = '/etc/apt/sources.list.d/gocd.list'
+    srcs_list = "/etc/apt/sources.list.d/gocd.list"
     if not exists(srcs_list):
-        append(srcs_list, 'deb https://download.gocd.org /', use_sudo=True)
-        sudo('curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo apt-key add -')
+        append(srcs_list, "deb https://download.gocd.org /", use_sudo=True)
+        sudo("curl https://download.gocd.org/GOCD-GPG-KEY.asc | sudo apt-key add -")
 
 
 def install_master1(*args, **kwargs):
-    apt_depends('go-server')
+    apt_depends("go-server")
 
 
 def install_slave2(*args, **kwargs):
-    apt_depends('go-agent')
+    apt_depends("go-agent")
 
 
 def configure_nginx3(*args, **kwargs):
     nginx.install_nginx0()
     nginx.setup_nginx_init1()
 
-    if kwargs.get('self_signed'):
+    if kwargs.get("self_signed"):
         self_signed0(use_sudo=True, **kwargs)
 
     server_block = utils.merge_into(
-        (lambda server_block: utils.apply_attributes(server_block,
-                                                     emit.secure_attr(kwargs['SSL_CERTOUT'],
-                                                                      kwargs['SSL_KEYOUT'])
-                                                     ) if kwargs['LISTEN_PORT'] == 443 else server_block)(
-            emit.server_block(server_name=kwargs['SERVER_NAME'],
-                              listen=kwargs['LISTEN_PORT'])
+        (
+            lambda server_block: utils.apply_attributes(
+                server_block,
+                emit.secure_attr(kwargs["SSL_CERTOUT"], kwargs["SSL_KEYOUT"]),
+            )
+            if kwargs["LISTEN_PORT"] == 443
+            else server_block
+        )(
+            emit.server_block(
+                server_name=kwargs["SERVER_NAME"], listen=kwargs["LISTEN_PORT"]
+            )
         ),
-        emit.api_proxy_block('/go', 'https://localhost:8154')
+        emit.api_proxy_block("/go", "https://localhost:8154"),
     )
 
     sio = StringIO()
-    sio.write(dumps(
-        loads(emit.redirect_block(server_name=kwargs['SERVER_NAME'], port=80)) + server_block
-        if kwargs['LISTEN_PORT'] == 443 else server_block
-    ))
+    sio.write(
+        dumps(
+            loads(emit.redirect_block(server_name=kwargs["SERVER_NAME"], port=80))
+            + server_block
+            if kwargs["LISTEN_PORT"] == 443
+            else server_block
+        )
+    )
 
-    put(sio,
-        '/etc/nginx/sites-enabled/{server_name}'.format(server_name=kwargs['SERVER_NAME']),
-        use_sudo=True, )
+    put(
+        sio,
+        "/etc/nginx/sites-enabled/{server_name}".format(
+            server_name=kwargs["SERVER_NAME"]
+        ),
+        use_sudo=True,
+    )
 
-    return restart_systemd('nginx')
+    return restart_systemd("nginx")
